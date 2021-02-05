497f3ba73dd08c2123aa8daddfc2f38a43d7554d
Calixt Charlebois <cj.charlebois@baycollege.edu>
2021-02-05 15:14:17 -0500

    Added Project Documentation

--- testProgram.cpp
+++ testProgram.cpp
@@ -1,10 +1,13 @@
-// **********************************************************************************
-// *                             Test Program !                                     *
-// **********************************************************************************
+/*
+Programmers: Calixt Charlebois, Jeremiah Clifford, Joshua Hallfrish, and Tyler Williams.
+Assignment:  Group Project
+Due Date:    April 18th, 2021.
+Description: 
+*/
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "CSCI 122";
+    std::cout << "CSCI 122 Group Project";
 }
